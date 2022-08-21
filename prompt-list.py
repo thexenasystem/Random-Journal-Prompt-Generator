29adb82645f85f60361be0b0af037976eb6397a6
thexenasystem <thexenasystem@gmail.com>
2022-08-21 11:59:55 -0400

    changed is not to != to make compy happy but that's not pythonic i don't think

--- prompt-list.py
+++ prompt-list.py
@@ -1,14 +1,14 @@
 import random
 import string
 
-journal_prompts = ["This is your first journal prompt", "This is the second prompt. It has two sentences.", "This is the third prompt.", "This is the fourth prompt.", "This is the fifth prompt."]
+journal_prompts = ["Am I doing this for myself?", "What is my why?", "What is the long-term potential of this habit?", "Is it realistic and achievable?", "Do I have a plan?", "What challenges am I likely to encounter?", "How will I keep myself accountable?"]
 # new_prompt = ''
 
 #def get_new_prompt():
     #selected_prompt = random.choice(journal_prompts)
     #return selected_prompt
 
-print("Would you like a new journal prompt? Push 'Y' for yes and 'N' for no.")
+print("Would you like a new journal prompt? Push Y for yes and N for no.")
 response = input()
 
 if response is 'Y':
@@ -17,5 +17,5 @@ if response is 'Y':
 elif response is 'N':
     print("You have selected no.")
 
-elif response is not 'Y' and response is not 'N':
+elif response != 'Y' and response != 'N':
     print('Error. Incorrect input. Please hit "Y" for yes and "N" for no.')
