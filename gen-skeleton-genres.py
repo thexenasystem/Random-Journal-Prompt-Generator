7c5643dea853bf8d2df717824ea8e2e775c6cdb0
thexenasystem <thexenasystem@gmail.com>
2022-08-29 17:18:52 -0400

    code runs but does not recognize input for genre

---
+++ gen-skeleton-genres.py
@@ -0,0 +1,86 @@
+from lib2to3.pgen2.pgen import generate_grammar
+import string
+import random
+
+#og code
+#journal_prompts = ["Am I doing this for myself?", "What is my why?", "What is the long-term potential of this habit?", "Is it realistic and achievable?", "Do I have a plan?", "What challenges am I likely to encounter?", "How will I keep myself accountable?"]
+
+#list of genres and each element in the genre is itself a list of prompts for that genre
+
+daily = [["daily prompt one"], ["daily prompt two"], ["daily prompt three"], ["daily prompt four"], ["daily prompt five"]]
+weekly = [["weekly prompt one"], ["weekly prompt two"], ["weekly prompt three"], ["weekly prompt four"], ["weekly prompt five"]]
+monthly = [["monthly prompt one"], ["monthly prompt two"], ["monthly prompt three"], ["monthly prompt four"], ["monthly prompt five"]]
+end_of_year = [["eoy prompt one"], ["eoy prompt two"], ["eoy prompt three"], ["eoy prompt four"], ["eoy prompt five"]]
+depression = ["dep prompt one"], ["dep prompt two"], ["dep prompt three"], ["dep prompt four"], ["dep prompt five"]
+anxiety = [["anx prompt one"], ["anx prompt two"], ["anx prompt three"], ["anx prompt four"], ["anx prompt five"]]
+gratitude = [["grat prompt one"], ["grat prompt two"], ["grat prompt three"], ["grat prompt four"], ["grat prompt five"]]
+self_development = [["self dev prompt one"], ["self dev prompt two"], ["self dev prompt three"], ["self dev prompt four"], ["self dev prompt five"]]
+self_love = [["self love prompt one"], ["self love prompt two"], ["self love prompt three"], ["self love prompt four"], ["self love prompt five"]]
+morning = [["am prompt one"], ["am prompt two"], ["am prompt three"], ["am prompt four"], ["am prompt five"]]
+evening = [["pm prompt one"], ["pm prompt two"], ["pm prompt three"], ["pm prompt four"], ["pm prompt five"]]
+grief = [["grief prompt one"], ["grief prompt two"], ["grief prompt three"], ["grief prompt four"], ["grief prompt five"]]
+mid_day = [["noon prompt one"], ["noon prompt two"], ["noon prompt three"], ["noon prompt four"], ["noon prompt five"]]
+confidence = [["conf prompt one"], ["conf prompt two"], ["conf prompt three"], ["conf prompt four"], ["conf prompt five"]]
+
+#list of genres with prompt lists for each genre
+journal_prompt_genres = [[daily],
+[weekly], [monthly], [end_of_year], [depression], [anxiety], [gratitude], [self_development], [self_love], [morning], [evening], [grief], [mid_day], [confidence]]
+
+#print("Would you like a new journal prompt? Push Y for yes and N for no.")
+#response = input()
+
+#if response is 'Y':
+print("What genre are you looking for? Your choices are: daily, weekly, monthly, end of year, depression, anxiety, gratitude, self development, self love, morning, evening, grief, mid-day, and confidence.")
+genre = input()
+    
+if genre is "daily":
+    print(random.choice(daily))
+
+elif genre is "weekly":
+    print(random.choice(weekly))
+
+elif genre is "monthly":
+    print(random.choice(monthly))
+
+elif genre is "end of year":
+    print(random.choice(end_of_year))
+
+elif genre is "depression":
+    print(random.choice(depression))
+
+elif genre is "anxiety":
+    print(random.choice(anxiety))
+
+elif genre is "gratitude":
+    print(random.choice(gratitude))
+
+elif genre is "self development":
+    print(random.choice(self_development))
+
+elif genre is "self love":
+    print(random.choice(self_love))
+
+elif genre is "morning":
+    print(random.choice(morning))
+
+elif genre is "evening":
+    print(random.choice(evening))
+    
+elif genre is "grief":
+    print(random.choice(grief))
+
+elif genre is "mid-day":
+    print(random.choice(mid_day))
+
+elif genre is "confidence":
+    print(random.choice(confidence))
+
+else: 
+    print("I'm sorry. That is not one of our available genres.")
+
+
+#elif response is 'N':
+    #print("You have selected no.")
+
+#elif response != 'Y' and response != 'N':
+    #print('Error. Incorrect input. Please hit "Y" for yes and "N" for no.')
