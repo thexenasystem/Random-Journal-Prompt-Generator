5ca6a8aee77e991935eb1473298e2c9f754262c9
thexenasystem <thexenasystem@gmail.com>
2022-08-30 16:33:34 -0400

    add bare bones pseudo for text field displaying the new prompt

--- generator-gui.py
+++ generator-gui.py
@@ -20,5 +20,15 @@ B.place(x = 50,y = 50)
 
 
 #text field displaying the new prompt
+root = Tk()
+text = Text(root)
+text.insert(INSERT, getNewPrompt)
+text.pack()
+
+text.tag_add("here", "1.0", "1.4") #change this placement
+text.tag_add("start", "1.8", "1.13") #change this placement
+text.tag_config("here", background = "white", foreground = "black")
+text.tag_config("start", background = "white", foreground = "black")
+root.mainloop()
 
 top.mainloop()
