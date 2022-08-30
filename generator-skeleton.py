3a742963759f1f9097ed75c4e3a2cdb0305a4076
thexenasystem <thexenasystem@gmail.com>
2022-08-30 12:47:17 -0400

    replace prompts list with new, larger list

--- generator-skeleton.py
+++ generator-skeleton.py
@@ -1,8 +1,24 @@
 import random
 import string
 
-journal_prompts = ["Am I doing this for myself?", "What is my why?", "What is the long-term potential of this habit?", "Is it realistic and achievable?", "Do I have a plan?", "What challenges am I likely to encounter?", "How will I keep myself accountable?"]
-# new_prompt = ''
+journal_prompts = [["this is prompt one"], 
+    ["this is prompt two"], 
+    ["look! it's the third prompt!"], 
+    ["and this is the fourth prompt"],
+    ["gonna make so many promppttsssssss"],
+    ["this list is gonna be huge"],
+    ["but not in a trump way"],
+    ["how is your day going?"],
+    ["what would you like to change about today?"],
+    ["how are you feeling this morning?"],
+    ["what are five things you love about yourself?"],
+    ["what is a pet peeve of yours?"],
+    ["what is the most frustrating thing about your job?"],
+    ["what is the best thing about your job?"],
+    ["even more journal prompts"],
+    ["all of the journal prompts"],
+    ["can't decide if this should be in it's own file or the main file"],
+    ["chips are really good"]]
 
 #def get_new_prompt():
     #selected_prompt = random.choice(journal_prompts)
