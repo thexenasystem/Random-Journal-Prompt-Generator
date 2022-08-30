28eca5301c708e40fa2aa8a8a5bfea8cc8511abd
thexenasystem <thexenasystem@gmail.com>
2022-08-30 12:41:00 -0400

    change prompt list from genres to one big list

--- prompt-list.py
+++ prompt-list.py
@@ -1,24 +1,21 @@
 import string
 
 #og code
-journal_prompts = ["Am I doing this for myself?", "What is my why?", "What is the long-term potential of this habit?", "Is it realistic and achievable?", "Do I have a plan?", "What challenges am I likely to encounter?", "How will I keep myself accountable?"]
-
-#list of genres and each element in the genre is itself a list of prompts for that genre
-
-daily = [["daily prompt one"], ["daily prompt two"], ["daily prompt three"], ["daily prompt four"], ["daily prompt five"]]
-weekly = [["weekly prompt one"], ["weekly prompt two"], ["weekly prompt three"], ["weekly prompt four"], ["weekly prompt five"]]
-monthly = [["monthly prompt one"], ["monthly prompt two"], ["monthly prompt three"], ["monthly prompt four"], ["monthly prompt five"]]
-end_of_year = [["eoy prompt one"], ["eoy prompt two"], ["eoy prompt three"], ["eoy prompt four"], ["eoy prompt five"]]
-depression = ["dep prompt one"], ["dep prompt two"], ["dep prompt three"], ["dep prompt four"], ["dep prompt five"][]
-anxiety = [["anx prompt one"], ["anx prompt two"], ["anx prompt three"], ["anx prompt four"], ["anx prompt five"]]
-gratitude = [["grat prompt one"], ["grat prompt two"], ["grat prompt three"], ["grat prompt four"], ["grat prompt five"]]
-self_development = [["self dev prompt one"], ["self dev prompt two"], ["self dev prompt three"], ["self dev prompt four"], ["self dev prompt five"]]
-self_love = [["self love prompt one"], ["self love prompt two"], ["self love prompt three"], ["self love prompt four"], ["self love prompt five"]]
-morning = [["am prompt one"], ["am prompt two"], ["am prompt three"], ["am prompt four"], ["am prompt five"]]
-evening = [["pm prompt one"], ["pm prompt two"], ["pm prompt three"], ["pm prompt four"], ["pm prompt five"]]
-grief = ["grief prompt one"], ["grief prompt two"], ["grief prompt three"], ["grief prompt four"], ["grief prompt five"][]
-mid_day = [["noon prompt one"], ["noon prompt two"], ["noon prompt three"], ["noon prompt four"], ["noon prompt five"]]
-confidence = [["conf prompt one"], ["conf prompt two"], ["conf prompt three"], ["conf prompt four"], ["conf prompt five"]]
-
-journal_prompt_genres = [[daily],
-[weekly], [monthly], [end_of_year], [depression], [anxiety], [gratitude], [self_development], [self_love], [morning], [evening], [grief], [mid_day], [confidence]]
+journal_prompts = [["this is prompt one"], 
+    ["this is prompt two"], 
+    ["look! it's the third prompt!"], 
+    ["and this is the fourth prompt"],
+    ["gonna make so many promppttsssssss"],
+    ["this list is gonna be huge"],
+    ["but not in a trump way"],
+    ["how is your day going?"],
+    ["what would you like to change about today?"],
+    ["how are you feeling this morning?"],
+    ["what are five things you love about yourself?"],
+    ["what is a pet peeve of yours?"],
+    ["what is the most frustrating thing about your job?"],
+    ["what is the best thing about your job?"],
+    ["even more journal prompts"],
+    ["all of the journal prompts"],
+    ["can't decide if this should be in it's own file or the main file"],
+    ["chips are really good"]]
