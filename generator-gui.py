6e698aa7a438089634e6dc25c3123c21c6418852
thexenasystem <thexenasystem@gmail.com>
2022-09-01 16:12:35 -0400

    more tkinter example code

--- generator-gui.py
+++ generator-gui.py
@@ -1,7 +1,8 @@
 import tkinter
 
 from tkinter import *
-from tkinter.ttk import *
+from tkinter import ttk
+from tkinter import messagebox
 
 top = tkinter.Tk()
 
@@ -16,8 +17,7 @@ def getNewPrompt():
 
 B = Button(top, text = "New Journal Prompt", command = getNewPrompt)
 B.place(x = 50,y = 50)
-
-
+top.mainloop()
 
 #text field displaying the new prompt
 root = Tk()
@@ -32,3 +32,41 @@ text.tag_config("start", background = "white", foreground = "black")
 root.mainloop()
 
 top.mainloop()
+
+# EXAMPLE CODE FROM TKINTER TUTORIAL  
+
+    #def calculate(*args):
+    #try:
+        #value = float(feet.get())
+        #meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    #except ValueError:
+        #pass
+
+#root = Tk()
+#root.title("Feet to Meters")
+
+#mainframe = ttk.Frame(root, padding="3 3 12 12")
+#mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+#root.columnconfigure(0, weight=1)
+#root.rowconfigure(0, weight=1)
+
+#feet = StringVar()
+#feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+#feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+#meters = StringVar()
+#ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+#ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+#ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+#ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+#ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+#for child in mainframe.winfo_children(): 
+    #child.grid_configure(padx=5, pady=5)
+
+#feet_entry.focus()
+#root.bind("<Return>", calculate)
+
+#root.mainloop()
