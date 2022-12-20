10ddad5755d4dad91d886ec3dcd1789a62a54aa4
thexenasystem <thexenasystem@gmail.com>
2022-12-20 17:32:41 -0500

    deleted journal_prompts list from generator window file

--- app/generator_window.py
+++ app/generator_window.py
@@ -8,27 +8,6 @@ from tkinter import messagebox
 import random
 import string
 
-"""
-journal_prompts = [["this is prompt one"], 
-    ["this is prompt two"], 
-    ["look! it's the third prompt!"], 
-    ["and this is the fourth prompt"],
-    ["gonna make so many promppttsssssss"],
-    ["this list is gonna be huge"],
-    ["but not in a trump way"],
-    ["how is your day going?"],
-    ["what would you like to change about today?"],
-    ["how are you feeling this morning?"],
-    ["what are five things you love about yourself?"],
-    ["what is a pet peeve of yours?"],
-    ["what is the most frustrating thing about your job?"],
-    ["what is the best thing about your job?"],
-    ["even more journal prompts"],
-    ["all of the journal prompts"],
-    ["can't decide if this should be in it's own file or the main file"],
-    ["chips are really good"]]
-"""
-
 def prompt_window():
     """Creates the prompt window and displays random journal prompt."""
 
