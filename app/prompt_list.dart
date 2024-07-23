7166ed567ee5bfc355b447525b9f875d64519f6e
thexenasystem <thexenasystem@gmail.com>
2024-07-22 21:58:31 -0400

    added missing prompt and TODO note

--- app/prompt_list.dart
+++ app/prompt_list.dart
@@ -1,5 +1,6 @@
-List ? journal_prompts = List.filled(15, null, growable: false) ["look! it's the third prompt!", 
-    journal_prompts[0]="and this is the fourth prompt";
+List ? journal_prompts = List.filled(16, null, growable: false) [
+  ///TO DO: wtf is the error below of can't assign string to int for first object?
+    journal_prompts[0]="DDDDD JJJJJJ KHALLEEDDDD ANOTHER ONE";
     journal_prompts[1]="gonna make so many promppttsssssss";
     journal_prompts[2]="this list is gonna be huge";
     journal_prompts[3]="but not in a trump way";
@@ -13,5 +14,6 @@ List ? journal_prompts = List.filled(15, null, growable: false) ["look! it's the
     journal_prompts[11]="even more journal prompts";
     journal_prompts[12]="all of the journal prompts";
     journal_prompts[13]="can't decide if this should be in it's own file or the main file";
-    journal_prompts[14]="chips are really good"];
-    
+    journal_prompts[14]="chips are really good";
+    journal_prompts[15]="look! it's another prompt!"
+]
