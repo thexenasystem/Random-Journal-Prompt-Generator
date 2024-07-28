e3b5112550b9ef86fae7cde356466417f3bb30c5
thexenasystem <thexenasystem@gmail.com>
2024-07-28 05:55:47 -0400

    fixed the list so it saves strings

--- app/prompt_list.dart
+++ app/prompt_list.dart
@@ -1,19 +1,16 @@
-List ? journal_prompts = List.filled(16, null, growable: false) [
-  //TO DO: wtf is the error below of can't assign string to int for first object?
-    journal_prompts[0]="DDDDD JJJJJJ KHALLEEDDDD ANOTHER ONE";
-    journal_prompts[1]="gonna make so many promppttsssssss";
-    journal_prompts[2]="this list is gonna be huge";
-    journal_prompts[3]="but not in a trump way";
-    journal_prompts[4]="how is your day going?";
-    journal_prompts[5]="what would you like to change about today?";
-    journal_prompts[6]="how are you feeling this morning?";
-    journal_prompts[7]="what are five things you love about yourself?";
-    journal_prompts[8]="what is a pet peeve of yours?";
-    journal_prompts[9]="what is the most frustrating thing about your job?";
-    journal_prompts[10]="what is the best thing about your job?";
-    journal_prompts[11]="even more journal prompts";
-    journal_prompts[12]="all of the journal prompts";
-    journal_prompts[13]="can't decide if this should be in it's own file or the main file";
-    journal_prompts[14]="chips are really good";
-    journal_prompts[15]="look! it's another prompt!"
+var journal_prompts = [
+     "gonna make so many promppttsssssss",
+     "this list is gonna be huge",
+     "but not in a trump way",
+     "how is your day going?",
+     "what would you like to change about today?",
+     "how are you feeling this morning?",
+     "what are five things you love about yourself?",
+     "what is a pet peeve of yours?",
+     "what is the most frustrating thing about your job?",     
+     "what is the best thing about your job?",
+     "all of the journal prompts",
+     "can't decide if this should be in it's own file or the main file",
+     "chips are really good",
+     "look! it's another prompt!"
 ];
