6939cc1ee968b08beb25d23f17d73ad0dcfba85c
thexenasystem <thexenasystem@gmail.com>
2024-07-30 18:25:07 -0400

    pseudo code place holder for function to return rando element from list

---
+++ app/get_new_prompt.py
@@ -0,0 +1,16 @@
+from prompt_list import journal_prompts
+import random
+
+def get_new_journal_prompt(journal_prompts):
+    """Returns a new random journal prompt."""
+    
+    #shuffle prompts list
+    if string = old prompt {
+        shuffle
+        /*AND/OR*/
+        skip
+    }
+    #TODO: return random(journal_prompts[0])  from list after shuffling
+    
+
+print(journal_prompts[0])
