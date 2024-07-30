7668ba40d3638b05d1790ce3a26d30c9790b870d
thexenasystem <thexenasystem@gmail.com>
2024-07-30 18:27:51 -0400

    pseudo code added to proper file and python prompt file deleted again

--- app/get_new_prompt.dart
+++ app/get_new_prompt.dart
@@ -3,8 +3,16 @@ import journal_prompts
 void get_new_journal_prompt(journal_prompts) {
   //returns random new journal prompt
   // TODO return random object from journal prompt list
-  //search array journal_prompts
+  //shuffle journal_prompts elements
+      #shuffle prompts list
+    if string = old prompt {
+        shuffle
+        /*AND/OR*/
+        skip
+    }
+    #TODO: return random(journal_prompts[0])  from list after shuffling
   //return random object from array
+
   //save returned object to var new_journal_prompt
   return new_journal_prompt
 };
